4a601c42eca7bfceaebe2939aac8a8be53e2a3ae
emmadenney <emma_denney@hotmail.com>
2024-01-07 19:09:27 +0000

    adding revalidation

--- app/api/revalidation/route.js
+++ app/api/revalidation/route.js
@@ -4,9 +4,13 @@ import { revalidateTag } from "next/cache";
 export async function POST(request) {
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get("x-vercel-reval-key");
+  const envSecret = process.env.CONTENTFUL_REVALIDATE_SECRET;
 
   if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
-    return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
+    return NextResponse.json(
+      { message: "Invalid secret", secret, envSecret },
+      { status: 401 }
+    );
   }
 
   revalidateTag("projects");
