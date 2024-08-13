16433d21b4de5194f7511b6fdbc3e8b281862043
emmadenney <emma_denney@hotmail.com>
2024-08-13 09:06:07 +0100

    switching

--- app/page.tsx
+++ app/page.tsx
@@ -7,43 +7,29 @@ export default async function HomePage() {
   const { isEnabled } = draftMode();
   const bookDesigns = await getAllBookDesigns(isEnabled);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <section className="w-full pt-12">
-        <div className="mx-auto container space-y-12 px-4 md:px-6">
-          <div className="flex flex-col items-center justify-center space-y-4 text-center">
-            <div className="space-y-2">
-              <p
-                className="max-w-[900px] md:text-xl/relaxed lg:text-xl/relaxed xl:text-xl/relaxed body-font"
-                style={{ color: "#6f4568" }}
-              >
-                COVER DESIGN | GRAPHICS | ILLUSTRATION | FINE ART
-              </p>
-            </div>
+    <main>
+      <div className="cover-text-container">
+        <p className="cover-text main-font">
+          COVER DESIGN | GRAPHICS | ILLUSTRATION | FINE ART
+        </p>
+      </div>
+      <div className="book-list-container">
+        {bookDesigns.map((bookDesign: any) => (
+          <div key={bookDesign.sys.id} className="book-list-item">
+            <Link href={`/book-designs/${bookDesign.slug}`}>
+              <div className="book-cover-container">
+                <Image
+                  alt="placeholder"
+                  className="book-cover"
+                  height="600"
+                  src={bookDesign.thumbnail.url}
+                  width="400"
+                />
+              </div>
+            </Link>
           </div>
-          <div className="space-y-12">
-            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-              {bookDesigns.map((bookDesign: any) => (
-                <div
-                  key={bookDesign.sys.id}
-                  className="h-full flex flex-col rounded-lg"
-                >
-                  <Link href={`/book-designs/${bookDesign.slug}`}>
-                    <div className="book-cover-container">
-                      <Image
-                        alt="placeholder"
-                        className="book-cover"
-                        height="600"
-                        src={bookDesign.thumbnail.url}
-                        width="400"
-                      />
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </section>
+        ))}
+      </div>
     </main>
   );
 }
