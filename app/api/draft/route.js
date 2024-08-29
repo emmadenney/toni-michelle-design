d33038040d9cce3abacf2e9827b678d79cd5e089
emmadenney <emma_denney@hotmail.com>
2024-08-29 08:38:57 +0100

    using getProject

--- app/api/draft/route.js
+++ app/api/draft/route.js
@@ -1,26 +1,26 @@
-// import { getProject } from "../../../lib/api";
-// import { draftMode } from "next/headers";
-// import { redirect } from "next/navigation";
+import { getProject } from "../../../lib/api";
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
 
-// export async function GET(request) {
-//   const { searchParams } = new URL(request.url);
-//   const secret = searchParams.get("secret");
-//   const slug = searchParams.get("slug");
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+  const slug = searchParams.get("slug");
 
-//   if (!secret || !slug) {
-//     return new Response("Missing parameters", { status: 400 });
-//   }
+  if (!secret || !slug) {
+    return new Response("Missing parameters", { status: 400 });
+  }
 
-//   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
-//     return new Response("Invalid token", { status: 401 });
-//   }
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
+    return new Response("Invalid token", { status: 401 });
+  }
 
-//   const project = await getProject(slug);
+  const project = await getProject(slug, true);
 
-//   if (!project) {
-//     return new Response("Project not found", { status: 404 });
-//   }
+  if (!project) {
+    return new Response("Project not found", { status: 404 });
+  }
 
-//   draftMode().enable();
-//   redirect(`/projects/${project.slug}`);
-// }
+  draftMode().enable();
+  redirect(`/projects/${project.slug}`);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
-import { getAllBookDesigns } from "../lib/api";
+import { getProject } from "../lib/api";
 import { draftMode } from "next/headers";
-import book1 from "./assets/1.png";
-import book2 from "./assets/2.png";
-import book3 from "./assets/3.png";
 
 export default async function HomePage() {
   const { isEnabled } = draftMode();
-  const bookDesigns = await getAllBookDesigns(isEnabled);
-  // const bookDesigns = [book1, book2, book3];
+  // const bookDesigns = await getAllBookDesigns(isEnabled);
+
+  const project = await getProject("books", isEnabled);
+
+  if (!project) {
+    return <p>No project found</p>;
+  }
+
+  const { projectsCollection } = project;
+
   return (
     <main>
       <div className="cover-text-container">
@@ -17,12 +22,12 @@ export default async function HomePage() {
         </p>
       </div>
       <div className="book-list-container">
-        {bookDesigns.map((bookDesign: any) => (
+        {projectsCollection.items.map((bookDesign: any) => (
           <div key={bookDesign.sys.id} className="book-list-item">
             {/* <Link href={`/book-designs/${bookDesign.slug}`}> */}
             <div className="book-cover-container">
               <Image
-                alt="placeholder"
+                alt={bookDesign.title}
                 className="book-cover"
                 height="600"
                 src={bookDesign.thumbnail.url}
