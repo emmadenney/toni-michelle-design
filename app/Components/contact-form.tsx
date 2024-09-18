0833c8909041b1bb4e5a2f3a4a049cad32f1436d
emmadenney <emma_denney@hotmail.com>
2024-09-18 08:35:06 +0100

    fixed mobile view hamburger menu

--- app/Components/contact-form.tsx
+++ app/Components/contact-form.tsx
@@ -1,28 +1,53 @@
+"use client";
+
+import { useState } from "react";
+
 export default async function ContactForm() {
-  const submitFormHandler = (event: any) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
     // send email to tmichelle
   };
 
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={handleSubmit}>
       <div className="contact-form-item main-font">
         <label htmlFor="name">Name</label>
-        <input name="name" type="text"></input>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
       </div>
       <div className="contact-form-item">
         <label htmlFor="email">Email</label>
-        <input name="email" type="text"></input>
+        <input
+          name="email"
+          type="text"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
       </div>
       <div className="contact-form-item">
         <label htmlFor="message">Message</label>
-        <textarea name="message"></textarea>
+        <textarea
+          name="message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></textarea>
       </div>
       <div className="button-container">
-        <button
-          // onClick={submitFormHandler}
-          className="contact-form-submit-btn main-font"
-        >
+        <button type="button" className="contact-form-submit-btn main-font">
           SUBMIT
         </button>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Navbar from "./Components/navbar";
 import Footer from "./Components/footer";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
