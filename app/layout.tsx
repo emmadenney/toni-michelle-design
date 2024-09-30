20ceccdfc8180d751b8c70d1646853d97e7437f9
emmadenney <emma_denney@hotmail.com>
2024-09-30 09:09:02 +0100

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </Head>
       <body>
         <Navbar />
         {children}
